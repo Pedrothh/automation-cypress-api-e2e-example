3d571e8b4b522a6a3c73a32e96ba26df9c4c6545
Pedro Thomé <pedro.th.dev@gmail.com>
2024-06-18 21:59:47 -0300

    add login validation scenario

--- cypress/e2e/pages/LoginPage.js
+++ cypress/e2e/pages/LoginPage.js
@@ -1,13 +1,28 @@
+import BasePage from "./BasePage";
+
 const userNameInput = 'input[name="userName"]'
+const passwordInput = 'input[name="password"]'
+const submitBtn = 'input[name="submit"]'
+const loginSuccessfullyMessageMapping = 'body > div:nth-child(5) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > h3:nth-child(1)'
 
 
-class LoginPage {
+
+class LoginPage extends BasePage {
 
   typeUserName(username) {    
-    cy.get(userNameInput).click();
-    cy.get(userNameInput).clear();
-    cy.get(userNameInput).type(username)
-    return this;
+    this.limparEPreencherCampo(userNameInput, username);
+  }
+
+  typePassword(password) {    
+    this.limparEPreencherCampo(passwordInput, password);
+  }
+
+  pressSubmit() {    
+    this.click(submitBtn);
+  }
+
+  shouldSeeTheMessageLoginSuccessfully(loginSuccessfullyMessage) {    
+    this.validarText(loginSuccessfullyMessageMapping, loginSuccessfullyMessage);
   }
 }
 
